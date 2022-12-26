eaade5ccd8f9f5e55f1a9128bb7a81a61992b9c0
Ubuntu <ubuntu@ip-172-31-42-255.us-west-2.compute.internal>
2022-12-26 05:48:18 +0000

    (Add) Gaussian noise and DP wrapper are added.

---
+++ shaper/tests/gaussian_test/test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../gaussian.h"
+
+int main(int argc, char *argv[])
+{
+  if(argc != 2){
+    printf("Usage: %s <number of samples>\n", argv[0]);
+  }
+
+  int sample_num = atoi(argv[1]);
+  // create a file to save the samples
+  FILE *fp = fopen("samples.txt", "w");
+  if(fp == NULL){
+    printf("Error: cannot open file samples.txt\n");
+    return -1;
+  }
+
+  double mu = 1.3;
+  double sigma = 7;
+
+  for (int i = 0; i < sample_num; i++){
+    double sample = gaussian(mean, variance);
+    fprintf(fp, "%f\n", sample);
+  }
+  fclose(fp);
+  return 0;
+
+}
