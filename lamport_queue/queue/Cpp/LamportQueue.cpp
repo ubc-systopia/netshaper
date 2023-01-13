524d9a12e5c61271ec6f3d1fd6cfeb9ece954e11
Rut Vora <contactme@rutvora.com>
2023-01-12 21:06:43 -0800

    Reduces actual queue size by 1 to avoid Front==Back when queue is full

--- lamport_queue/queue/Cpp/LamportQueue.cpp
+++ lamport_queue/queue/Cpp/LamportQueue.cpp
@@ -5,28 +5,28 @@
 #include "LamportQueue.hpp"
 
 LamportQueue::LamportQueue() {
-    front_ = 0;
-    back_ = 0;
-    cached_front_ = 0;
-    cached_back_ = 0;
+  front_ = 0;
+  back_ = 0;
+  cached_front_ = 0;
+  cached_back_ = 0;
 }
 
 // LamportQueue::~LamportQueue() {
 //   delete data_;
 // }
 
-int LamportQueue::push(uint8_t* elem, size_t elem_size){
+int LamportQueue::push(uint8_t *elem, size_t elem_size) {
   assert(elem_size < BUFFER_SIZE && elem_size > 0);
 
   size_t b, f;
   b = this->back_.load(std::memory_order_relaxed);
   f = this->cached_front_;
-  int free_space = this->get_free_space_local(f, b);
-  if(free_space <= elem_size){
+  size_t free_space = this->get_free_space_local(f, b);
+  if (free_space < elem_size) {
     this->cached_front_ = f = this->front_.load(std::memory_order_acquire);
   }
   free_space = this->get_free_space_local(f, b);
-  if(free_space <= elem_size){
+  if (free_space < elem_size) {
     return -1;
   }
   std::memcpy(this->data_ + b, elem, elem_size);
@@ -34,18 +34,18 @@ int LamportQueue::push(uint8_t* elem, size_t elem_size){
   return 0;
 }
 
-int LamportQueue::pop(uint8_t* elem, size_t elem_size){
+int LamportQueue::pop(uint8_t *elem, size_t elem_size) {
   assert(elem_size < BUFFER_SIZE && elem_size > 0);
 
   size_t b, f;
   f = this->front_.load(std::memory_order_relaxed);
   b = this->cached_back_;
-  int queue_size = this->get_queue_size_local(f, b);
-  if(queue_size < elem_size){
+  size_t queue_size = this->get_queue_size_local(f, b);
+  if (queue_size < elem_size) {
     this->cached_back_ = b = this->back_.load(std::memory_order_acquire);
   }
   queue_size = this->get_queue_size_local(f, b);
-  if(queue_size < elem_size){
+  if (queue_size < elem_size) {
     return -1;
   }
   std::memcpy(elem, this->data_ + f, elem_size);
@@ -53,26 +53,27 @@ int LamportQueue::pop(uint8_t* elem, size_t elem_size){
   return 0;
 }
 
-int LamportQueue::size(){
+size_t LamportQueue::size() {
   size_t f = this->front_.load(std::memory_order_relaxed);
   size_t b = this->back_.load(std::memory_order_acquire);
   return this->get_queue_size_local(f, b);
 }
-int LamportQueue::free_space(){
+
+size_t LamportQueue::free_space() {
   size_t f = this->front_.load(std::memory_order_relaxed);
   size_t b = this->back_.load(std::memory_order_acquire);
   return this->get_free_space_local(f, b);
 }
 
-int LamportQueue::mod(int a, int b){
-  int r = a % b;
+size_t LamportQueue::mod(size_t a, size_t b) {
+  ssize_t r = a % b;
   return r < 0 ? r + b : r;
 }
 
-int LamportQueue::get_free_space_local(size_t f, size_t b){
-  return BUFFER_SIZE - this->mod((b-f) , BUFFER_SIZE);
-} 
+size_t LamportQueue::get_free_space_local(size_t f, size_t b) {
+  return BUFFER_SIZE - this->mod((b - f), BUFFER_SIZE) - 1;
+}
 
-int LamportQueue::get_queue_size_local(size_t f, size_t b){
-  return this->mod((b-f) , BUFFER_SIZE);
+size_t LamportQueue::get_queue_size_local(size_t f, size_t b) {
+  return this->mod((b - f), BUFFER_SIZE);
 }
--- lamport_queue/queue/Cpp/LamportQueue.hpp
+++ lamport_queue/queue/Cpp/LamportQueue.hpp
@@ -15,24 +15,31 @@
 #define BUFFER_SIZE 1000
 #define SHM_KEY 0x1234
 
-class LamportQueue{
-  public:
-    explicit LamportQueue();
-    // ~LmportQueue();
-    int push(uint8_t* elem, size_t elem_size);
-    int pop(uint8_t* elem, size_t elem_size);
-    int size();
-    int free_space();
-  private:
-    std::atomic<size_t> front_;
-    std::atomic<size_t> back_;
-    size_t cached_front_;
-    size_t cached_back_;
-    uint8_t data_[BUFFER_SIZE];
+class LamportQueue {
+public:
+  explicit LamportQueue();
 
-    int mod(int a, int b);
-    int get_queue_size_local(size_t f, size_t b);
-    int get_free_space_local(size_t f, size_t b);
+  // ~LmportQueue();
+  int push(uint8_t *elem, size_t elem_size);
+
+  int pop(uint8_t *elem, size_t elem_size);
+
+  size_t size();
+
+  size_t free_space();
+
+private:
+  std::atomic<size_t> front_;
+  std::atomic<size_t> back_;
+  size_t cached_front_;
+  size_t cached_back_;
+  uint8_t data_[BUFFER_SIZE]{};
+
+  size_t mod(size_t a, size_t b);
+
+  size_t get_queue_size_local(size_t f, size_t b);
+
+  size_t get_free_space_local(size_t f, size_t b);
 };
 
 #endif //MINESVPN_LAMPORTQUEUE_H
