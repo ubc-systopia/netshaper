d1f7caefb42fadeb6a0b6576548d2317150197d8
Rut Vora <contactme@rutvora.com>
2023-01-14 21:42:55 -0800

    Added instruction to checkout msquic submodule

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -8,7 +8,7 @@ CYAN='\033[0;36m'
 RED='\033[0;31m'
 OFF='\033[0m'
 
-echo -e "${YELLOW}Installing prerequisites (G++, openSSL-1.1)"
+echo -e "${YELLOW}Installing prerequisites (openSSL-1.1)"
 wget -O - https://mirror.cmt.de/archlinux/core/os/x86_64/openssl-1.1-1.1.1.s-4-x86_64.pkg.tar.zst | unzstd --stdout | tar -xvf -
 chmod 644 usr/lib/lib*
 cp usr/lib/lib* /usr/lib/x86_64-linux-gnu/
@@ -20,7 +20,7 @@ echo -e "${YELLOW}Downloading and installing msQUIC"
 apt-get install -y libmsquic libssl-dev
 ln -s /usr/lib/x86_64-linux-gnu/libmsquic.so.2 /usr/lib/x86_64-linux-gnu/libmsquic.so
 
-echo -e "${YELLOW}Downloading and installing ninja-build and cmake"
+echo -e "${YELLOW}Downloading and installing ninja-build"
 apt-get install -y ninja-build
 
 echo -e "${GREEN}Setup complete"
