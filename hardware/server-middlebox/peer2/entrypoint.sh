50e32646bdab4d037079493887bdadd2c40f478f
Amir Sabzi <97.amirsabzi@gmail.com>
2024-01-11 11:56:45 -0800

    (ADD) primary scripts for MB-2

---
+++ hardware/server-middlebox/peer2/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Starting tcpdump for $((TIMEOUT + 15)) seconds"
+timeout -s SIGINT $((TIMEOUT + 15)) tcpdump -s $MAX_CAPTURE_SIZE -w traces/$PCAP.pcap &
+
+echo "Starting Peer2 for $((TIMEOUT + 10)) seconds"
+timeout -s SIGINT $((TIMEOUT + 10)) /root/peer_2 $CONFIG > traces/$PCAP.log 2>&1
+
+# Wait for tcpdump to die
+sleep 7
