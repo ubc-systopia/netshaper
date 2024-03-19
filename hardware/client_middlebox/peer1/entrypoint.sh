23eda59ada2e698b8ebc7cd9798deb264f4a8cdb
Amir Sabzi <97.amirsabzi@gmail.com>
2024-03-19 13:36:40 -0700

    (FIX) No need to run tcpdump in the containers

--- hardware/client_middlebox/peer1/entrypoint.sh
+++ hardware/client_middlebox/peer1/entrypoint.sh
@@ -3,8 +3,8 @@
 #sed "s/\"peer2\"/\"$PEER\"/" $CONFIG > config2.json
 sed "s/4567/$PEER_PORT/" $CONFIG > config2.json
 
-echo "Starting tcpdump for $((TIMEOUT + 15)) seconds"
-timeout -s SIGINT $((TIMEOUT + 15)) tcpdump -s $MAX_CAPTURE_SIZE -w traces/$PCAP.pcap &
+#echo "Starting tcpdump for $((TIMEOUT + 15)) seconds"
+#timeout -s SIGINT $((TIMEOUT + 15)) tcpdump -s $MAX_CAPTURE_SIZE -w traces/$PCAP.pcap &
 
 echo "Starting Peer1 for $((TIMEOUT + 10)) seconds"
 timeout -s SIGINT $((TIMEOUT + 10)) /root/peer_1 config2.json > traces/$PCAP.log 2>&1
--- hardware/server_middlebox/peer2/entrypoint.sh
+++ hardware/server_middlebox/peer2/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Starting tcpdump for $((TIMEOUT + 15)) seconds"
-timeout -s SIGINT $((TIMEOUT + 15)) tcpdump -s $MAX_CAPTURE_SIZE -w traces/$PCAP.pcap &
+#echo "Starting tcpdump for $((TIMEOUT + 15)) seconds"
+#timeout -s SIGINT $((TIMEOUT + 15)) tcpdump -s $MAX_CAPTURE_SIZE -w traces/$PCAP.pcap &
 
 echo "Starting Peer2 for $((TIMEOUT + 10)) seconds"
 timeout -s SIGINT $((TIMEOUT + 10)) /root/peer_2 $CONFIG > traces/$PCAP.log 2>&1
